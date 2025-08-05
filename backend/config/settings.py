31abf098457e9de4c7549de6a36806e49113d388
Nevzat Atalay <nevzatatalay79@gmail.com>
2025-08-05 18:46:40 +0300

    added toast structere

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,11 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
     'listings',
     'users',
-     
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
